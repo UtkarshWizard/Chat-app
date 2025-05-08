a3bb203e868e9a7fdc33f65a8509d7ad2312bf66
UtkarshWizard <utkarshr1214@gmail.com>
2025-05-08 22:52:48 +0530

    added middleware logic to ws and http

--- apps/HTTP-BE/src/middleware.ts
+++ apps/HTTP-BE/src/middleware.ts
@@ -2,18 +2,23 @@ import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "./config";
 
-interface JwtPayload {
-    userId: string;
-}
+
 
 export function middleware ( req: Request , res: Response , next: NextFunction ) {
     const token = req.headers["authorization"] ?? ""
     
-    const decode = jwt.verify( token , JWT_SECRET ) as JwtPayload
+    const decode = jwt.verify( token , JWT_SECRET )
 
+    if (typeof decode === "string") {
+        res.status(403).json({
+            message: "Unauthorized"
+        });
+        
+        return;
+    }
+    
     if (decode) {
         req.userId = decode.userId
-        next()
     } else {
         res.status(403).json({
             message: "Unauthorized"
